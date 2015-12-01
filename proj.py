8afc6fa30621ca9c34255eda25ae69e1cf964fe1
Simon Whittle <smw2ed@virginia.edu>
2015-12-01 15:48:53 -0500

    added shield graphic, and block and brace animations

--- proj.py
+++ proj.py
@@ -20,41 +20,37 @@ height = 800
 screen = rw.newDisplay(width, height, name)
 
 
-def detLegs(hyp, ang, X = 0, Y = 0):
+def detLegs(hyp, ang, X=0, Y=0):
 
     # determines the length of the legs of a right triangle based on
     # the "ang" angle and the "hyp" hypotenuse and adds them to
     # existing X and Y values
 
-    print("ang:", ang)
+    # print("ang:", ang)
 
     if ang == 0:
-        X = X
         Y -= hyp
 
-        print("X = X")
-        print("Y +=", hyp)
+        # print("X = X")
+        # print("Y +=", hyp)
 
     elif ang == 180:
-        X = X
         Y += hyp
 
-        print("X = X")
-        print("Y -=", hyp)
+        # print("X = X")
+        # print("Y -=", hyp)
 
     elif ang == 90:
         X += hyp
-        Y = Y
 
-        print("X +=", hyp)
-        print("Y = Y")
+        # print("X +=", hyp)
+        # print("Y = Y")
 
     elif ang == 270:
         X -= hyp
-        Y = Y
 
-        print("X -=", hyp)
-        print("Y = Y")
+        # print("X -=", hyp)
+        # print("Y = Y")
 
     elif ang > 0 and ang < 90:
 
@@ -63,8 +59,8 @@ def detLegs(hyp, ang, X = 0, Y = 0):
         X += hyp * math.cos(ang)
         Y -= hyp * math.sin(ang)
 
-        print("X +=", (hyp*(math.cos(ang))) )
-        print("Y +=", (hyp*(math.sin(ang))) )
+        # print("X +=", (hyp*(math.cos(ang))) )
+        # print("Y +=", (hyp*(math.sin(ang))) )
 
     elif ang > 90 and ang < 180:
 
@@ -73,8 +69,8 @@ def detLegs(hyp, ang, X = 0, Y = 0):
         X += hyp * math.cos(ang)
         Y += hyp * math.sin(ang)
 
-        print("X +=", (hyp*(math.cos(ang))) )
-        print("Y -=", (hyp*(math.sin(ang))) )
+        # print("X +=", (hyp*(math.cos(ang))) )
+        # print("Y -=", (hyp*(math.sin(ang))) )
 
     elif ang > 180 and ang < 270:
 
@@ -83,8 +79,8 @@ def detLegs(hyp, ang, X = 0, Y = 0):
         X -= hyp * math.cos(ang)
         Y += hyp * math.sin(ang)
 
-        print("X -=", (hyp*(math.cos(ang))) )
-        print("Y -=", (hyp*(math.sin(ang))) )
+        # print("X -=", (hyp*(math.cos(ang))) )
+        # print("Y -=", (hyp*(math.sin(ang))) )
 
     elif ang > 270 and ang < 360:
 
@@ -93,8 +89,8 @@ def detLegs(hyp, ang, X = 0, Y = 0):
         X -= hyp * math.cos(ang)
         Y -= hyp * math.sin(ang)
 
-        print("X -=", (hyp*(math.cos(ang))) )
-        print("Y +=", (hyp*(math.sin(ang))) )
+        # print("X -=", (hyp*(math.cos(ang))) )
+        # print("Y +=", (hyp*(math.sin(ang))) )
 
     else:
         print("\n!!!!!!!!!!\n")
@@ -103,6 +99,7 @@ def detLegs(hyp, ang, X = 0, Y = 0):
         print("\n!!!!!!!!!!\n")
 
     return (X, Y)
+
 
 class State(object):
     def __init__(self, screen, entities):
@@ -113,17 +110,33 @@ class State(object):
     def update(self):
         player = self.entities[0]
 
-        functDict = {119: (player.move, False, 5),
-                     97: (player.move, True, -5),
-                     115: (player.move, False, -5),
-                     100: (player.move, True, 5),
-                     113: (player.rotate, False, 5),
-                     101: (player.rotate, True, 5)}
+        functDict = {119: (player.move, False, 5),    # W
+                     97: (player.move, True, -5),     # A
+                     115: (player.move, False, -5),   # S
+                     100: (player.move, True, 5),     # D
+                     106: (player.rotate, False, 5),  # J
+                     108: (player.rotate, True, 5)}   # L
+        """
+        106:  # I
+        107:  # K
+        """
 
         for key in self.keys:
             if key in functDict:
                 # player.move(functDict[key])
                 functDict[key][0](functDict[key][1], functDict[key][2])
+
+        if (107 in self.keys) and (32 in self.keys) and (99 not in self.keys):
+            player.block = True
+            player.brace = False
+            player.rotVerts()
+        elif (107 in self.keys) and (32 in self.keys) and (99 in self.keys):
+            player.brace = True
+            player.rotVerts()
+        else:
+            player.block = False
+            player.brace = False
+            # player.rotVerts()
 
     def display(self):
         dw.fill(dw.black)
@@ -139,20 +152,22 @@ class State(object):
         return result
 
     def handleEvent(self, event):
+        """
         keyDict = {119: "W",
                    97: "A",
                    115: "S",
                    100: "D",
                    113: "Q",
                    101: "E"}
+        """
 
         if (event.type == pg.KEYDOWN):
-            #self.keys.append(keyDict[event.key])
+            # self.keys.append(keyDict[event.key])
             self.keys.append(event.key)
-            #print(event.key)
+            # print(event.key)
 
         elif (event.type == pg.KEYUP) and (event.key in self.keys):
-            #self.keys.remove(keyDict[event.key])
+            # self.keys.remove(keyDict[event.key])
             self.keys.remove(event.key)
             print(event.key)
 
@@ -181,6 +196,10 @@ class Entity(object):
         self.Ycoord = Ycoord
         self.head = 0
         self.health = 100
+        self.block = False
+        self.brace = False
+        self.shieldHead = self.head + 315 % 360
+        self.rotVerts()
         self.detVerts()
 
     def disp(self, screen):
@@ -188,13 +207,14 @@ class Entity(object):
         X = (self.Xcoord - 20)
         Y = (self.Ycoord - 20)
 
-        #arrowVerts = [[X, Y-20], [X+15, Y+20], [X, Y+15], [X-15, Y+20]]
+        # arrowVerts = [[X, Y-20], [X+15, Y+20], [X, Y+15], [X-15, Y+20]]
         pg.draw.circle(screen, BLUE, [X, Y], 40)
-        pg.draw.polygon(screen, RED, self.verts)
+        pg.draw.polygon(screen, RED, self.arrowVerts)
+        pg.draw.polygon(screen, WHITE, self.shieldVerts)
 
     def move(self, axis, mod):
-        #(Xmov, Ymov) = change
-        if axis == True:
+        # (Xmov, Ymov) = change
+        if axis is True:
             ang = (self.head + 90) % 360
         else:
             ang = self.head
@@ -204,15 +224,46 @@ class Entity(object):
         self.detVerts()
 
     def rotate(self, clock, ang):
-        if clock == False:
+        if clock is False:
             ang = -ang
         self.head = (self.head + ang) % 360
+        self.rotVerts()
+
+    def slash(self, targets):
+        pass
+
+    def rotVerts(self):
+        self.locArrowVerts = [detLegs(20, self.head), detLegs(25, (self.head + 135) % 360), detLegs(15, (self.head + 180) % 360), detLegs(25, (self.head + 225) % 360)]
+
+        if self.brace is False:
+            shieldSpace = 40
+        else:
+            shieldSpace = 50
+
+        if (self.block is True) or (self.brace is True):
+            self.shieldHead = self.head
+            print("iz tru, shieldHead =", self.shieldHead, "head =", self.head)
+        else:
+            self.shieldHead = (self.head + 300) % 360
+            print("iz false, shieldHead =", self.shieldHead, "head =", self.head)
+        (shieldX, shieldY) = detLegs(shieldSpace, self.shieldHead)
+
+        self.locShieldVerts = [detLegs(45, (self.shieldHead + 85) % 360, shieldX, shieldY), detLegs(45, (self.shieldHead + 95) % 360, shieldX, shieldY), detLegs(45, (self.shieldHead + 265) % 360, shieldX, shieldY), detLegs(45, (self.shieldHead + 275) % 360, shieldX, shieldY)]
+
         self.detVerts()
 
     def detVerts(self):
         X = (self.Xcoord - 20)
         Y = (self.Ycoord - 20)
-        self.verts = [detLegs(20, self.head, X, Y), detLegs(25, (self.head + 135) % 360, X, Y), detLegs(15, (self.head + 180) % 360, X, Y), detLegs(25, (self.head + 225) % 360, X, Y)]
+        self.arrowVerts = handleVerts(X, Y, self.locArrowVerts)
+        self.shieldVerts = handleVerts(X, Y, self.locShieldVerts)
+
+
+def handleVerts(X, Y, local):
+    newVerts = []
+    for vert in local:
+        newVerts.append((vert[0] + X, vert[1] + Y))
+    return newVerts
 
 player = Entity(150, 150, screen)
 state = State(screen, [player])
